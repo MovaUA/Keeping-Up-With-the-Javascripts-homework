751bfbe9757e5a79d35597ece03705927afda419
mova <valeriy.molchanov.77@gmail.com>
2020-10-10 14:55:20 +0300

    to-do-list: signup/login

---
+++ to-do-list/store.js
@@ -0,0 +1,23 @@
+function Store() {
+  this.users = new Map();
+
+  this.addUser = (user) => {
+    this.users.set(user.email, user);
+    this.save();
+  }
+
+  this.save = () => window.localStorage.setItem('users', JSON.stringify(Array.from(this.users.values())));
+
+
+  try {
+    const usersItem = window.localStorage.getItem('users');
+    const usersObject = JSON.parse(usersItem);
+    const usersArray = Array.isArray(usersObject) ? usersObject : [];
+    for (let user of usersArray) {
+      this.addUser(user);
+    }
+  } catch {
+  }
+}
+
+export default Store;
